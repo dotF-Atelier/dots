d8c65e46457ab4a32029c0cba508eb10e30bd64c
npchitman <npchitman@outlook.com>
2021-11-16 08:22:01 +0800

    -

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -25,7 +25,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-ayu-mirage)
+;; (setq doom-theme 'doom-ayu-mirage)
+(setq doom-theme 'my-doom-ayu-mirage)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
