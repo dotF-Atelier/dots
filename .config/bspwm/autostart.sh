91a59f4dc45ea38cd2dff358f0d381e6d3608470
npchitman <npchitman@outlook.com>
2021-11-20 14:42:16 +0800

    update shell color scheme; add portage porfile;

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+picom &
+fcitx5 &
+wal -R
+dunst &
+bash $HOME/.config/polybar/launch.sh &
+xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144.00 --output HDMI-A-0 --mode 1920x1080 --rate 60.00 --left-of DisplayPort-0
