a601cf8f9dda3b6b58919d10cc0147d33691a278
npchitman <npchitman@outlook.com>
2021-12-01 10:07:36 +0800

    -

--- .myprofile/bin/wacom_setup.sh
+++ .myprofile/bin/wacom_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # map monitor
-xsetwacom --set "Wacom Intuos Pro M Pen stylus" MapToOutput DisplayPort-0
+xsetwacom --set "Wacom Intuos Pro M Pen stylus" MapToOutput HDMI-A-0
 
 # turn Ring off
 xsetwacom --set "Wacom Intuos Pro M Pad pad" 'AbsWheelUp' 'button 0'
