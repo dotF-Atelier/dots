9553d344a3ec7f58abee0d9db294576a5ac57fe3
Kingstom <kingstom.chen@gmail.com>
2022-04-30 00:05:38 +0800

    -

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,6 +4,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" default))
  '(org-agenda-files '("/home/npchitman/workspace/notes/todo/recent.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
