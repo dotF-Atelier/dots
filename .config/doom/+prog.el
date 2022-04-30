5caf97dcf3708554e872a217a1c9e241f51a98b7
Kingstom <kingstom.chen@gmail.com>
2022-05-01 05:45:00 +0800

    -

--- .config/doom/+prog.el
+++ .config/doom/+prog.el
@@ -1,25 +1,69 @@
 ;;; +prog.el -*- lexical-binding: t; -*-
 ;;;
+
 ;; disable snipe mode
 (remove-hook 'doom-first-input-hook #'evil-snipe-mode)
 
+(after! company
+  (setq company-idle-delay 0.2)
+  (setq company-format-margin-function #'company-detect-icons-margin))
+
+(after! realgud (advice-remove #'realgud:terminate #'+debugger--cleanup-after-realgud-a))
+
+(add-to-list 'auto-mode-alist '("\\.inl\\'" . +cc-c-c++-objc-mode))
+(add-to-list 'auto-mode-alist '("\\.inc\\'" . +cc-c-c++-objc-mode))
+
+
 ;; lsp
-;; clangd
-(setq company-idle-delay 0.01)
-(setq lsp-headerline-breadcrumb-enable t)
-(setq lsp-log-io t)
-(setq lsp-clients-clangd-args '("-j=16"
-                                ;; "--all-scopes-completion"
-                                ;; "--clang-tidy"
-                                "--enable-config"
-                                "--background-index"
-                                "--completion-style=bundled"
-                                "--pch-storage=memory"
-                                "--header-insertion=never"
-                                "--log=verbose"
-                                "--header-insertion-decorators=0"
-                                ;; "--clang-tidy-checks='-*,clang-analyzer-*,readability-*,modernize-*,-clang-analyzer-osx*,-readability-identifier-length,-readability-braces-around-statements'"
-                                ))
+(setq +format-with-lsp nil)
+(after! lsp-mode
+  (setq lsp-headerline-breadcrumb-enable t
+        lsp-file-watch-threshold 4000
+        lsp-log-io nil
+        lsp-headerline-breadcrumb-segments '(file symbols)
+        lsp-imenu-index-symbol-kinds '(File Module Namespace Package Class Method Enum Interface
+                                            Function Variable Constant Struct Event Operator TypeParameter)
+        )
+  (dolist (dir '("[/\\\\]\\.ccls-cache\\'"
+                 "[/\\\\]\\.mypy_cache\\'"
+                 "[/\\\\]\\.pytest_cache\\'"
+                 "[/\\\\]\\.cache\\'"
+                 "[/\\\\]\\.clwb\\'"
+                 "[/\\\\]__pycache__\\'"
+                 "[/\\\\]bazel-bin\\'"
+                 "[/\\\\]bazel-code\\'"
+                 "[/\\\\]bazel-genfiles\\'"
+                 "[/\\\\]bazel-out\\'"
+                 "[/\\\\]bazel-testlogs\\'"
+                 "[/\\\\]third_party\\'"
+                 "[/\\\\]third-party\\'"
+                 "[/\\\\]buildtools\\'"
+                 "[/\\\\]out\\'"
+                 ))
+    (push dir lsp-file-watch-ignored-directories))
+  (setq lsp-clients-clangd-args '("-j=8"
+                                  ;; "--all-scopes-completion"
+                                  "--clang-tidy"
+                                  "--enable-config"
+                                  "--background-index"
+                                  "--completion-style=detailed"
+                                  "--pch-storage=memory"
+                                  "--header-insertion=never"
+                                  ;; "--log=verbose"
+                                  "--header-insertion-decorators=0"
+                                  ;; "--clang-tidy-checks='-*,clang-analyzer-*,readability-*,modernize-*,-clang-analyzer-osx*,-readability-identifier-length,-readability-braces-around-statements'"
+                                  ))
+)
+
+(after! lsp-ui
+  (setq lsp-ui-doc-enable nil
+        lsp-lens-enable nil
+        lsp-ui-sideline-enable nil
+        lsp-ui-doc-include-signature t
+        lsp-ui-doc-max-height 15
+        lsp-ui-doc-max-width 100))
+
+
 (after! lsp-clangd (set-lsp-priority! 'clangd 2))
 
 ;; eglot
