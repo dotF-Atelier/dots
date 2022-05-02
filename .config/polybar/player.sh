ddbd27013e09aef71335b465177b84dcfa02cd84
Kingstom <kingstom.chen@gmail.com>
2022-05-02 23:09:05 +0800

    display mpd volume

--- .config/polybar/player.sh
+++ .config/polybar/player.sh
@@ -4,6 +4,7 @@
 mpd_status=`mpc status`
 
 if [[ $mpd_status == *"[playing]"* ]]; then
+    echo -n $(echo -n $(mpc volume) | sed s/volume://) '| ' 
     echo `mpc current`
     exit
 fi
@@ -14,7 +15,7 @@ for player in $players
 do
         status=`playerctl -p $player status`
         if [[ $status == 'Playing' ]];then
-                output=`playerctl -p $player metadata title -f '{{artist}} - {{title}}'`
+		output=$(playerctl -p $player metadata title -f '{{artist}} - {{title}}')
                 break
         fi
 done
