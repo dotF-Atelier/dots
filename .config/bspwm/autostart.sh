275ea1f4d4c3f73fce67d6ef9cae28a3cdec62e1
Kingstom <kingstom.chen@gmail.com>
2022-07-24 23:04:00 +0800

    -

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -9,7 +9,7 @@ xset +fp $HOME/.config/local/share/fonts
 xset fp rehash
 xsetroot -cursor_name left_ptr&
 
-xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144.00 --output HDMI-A-0 --mode 1920x1080 --rate 60.00 --right-of DisplayPort-0
+xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144.00 --output HDMI-A-0 --mode 1920x1080 --rate 60.00 --left-of DisplayPort-0
 
 feh --bg-fill $HOME/Picture/wallpaper/sandworm.jpg --bg-fill $HOME/Picture/wallpaper/sandworm.jpg
 
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -37,11 +37,11 @@
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
-       ;;neotree           ; a project drawer, like NERDTree for vim
+       (neotree +lsp)           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;; tabs              ; a tab bar for Emacs
-       (treemacs +lsp)     ; a project drawer, like neotree but cooler
+       ;;(treemacs +lsp)     ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
